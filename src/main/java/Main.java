289b2223d209840d0698bd6c84f119f78e96f388
Wobble <72521183+PurpleVortX@users.noreply.github.com>
2024-03-06 15:37:13 +0000

    Adjusted Files and Started Application

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+
+  public static void main(String[] args)
+  {
+      JFrame frame = new JFrame("My Animatronic");
+      frame.setSize(300, 300);
+      frame.setLocation(5, 5);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      frame.setVisible(true);
+  }
+  
+}
